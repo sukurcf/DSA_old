0e3f33e9a38168fdf9cd5adc0cb646cc72d54dbc
sukurcf <sukurcf@gmail.com>
2021-03-20 23:40:59 +0530

    Added exercises for tree.

--- tree.py
+++ tree.py
@@ -1,13 +1,15 @@
 class TreeNode:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, name, designation):
+        self.name = name
+        self.designation = designation
         self.children = []
         self.parent = None
 
     def add_child(self, child):
         child.parent = self
         self.children.append(child)
+        return child
 
     def get_level(self):
         p = self.parent
@@ -17,38 +19,35 @@ class TreeNode:
             level += 1
         return level
 
-    def print_tree(self):
+    def print_tree(self, what_to_print, level):
+        if self.get_level() > level:
+            return
         prefix = " " * self.get_level() * 3 + "|__"
-        print(prefix + str(self.data))
+        if what_to_print == "name":
+            value = self.name
+        elif what_to_print == "designation":
+            value = self.designation
+        elif what_to_print == "both":
+            value = f"{self.name} ({self.designation})"
+        print(prefix + value)
         if self.children:
             for child in self.children:
-                child.print_tree()
+                child.print_tree(what_to_print, level)
 
 
 def build_product_tree():
-    root = TreeNode("Electronics")
-
-    laptops = TreeNode("Laptops")
-    laptops.add_child(TreeNode("Macbook"))
-    laptops.add_child(TreeNode("Surface book"))
-    laptops.add_child(TreeNode("Pavilion"))
-
-    cellphones = TreeNode("Cell phones")
-    cellphones.add_child(TreeNode("iPhone"))
-    cellphones.add_child(TreeNode("OnePlus"))
-
-    tvs = TreeNode("TVS")
-    tvs.add_child(TreeNode("LG"))
-    tvs.add_child(TreeNode("Onida"))
-    tvs.add_child(TreeNode("Samsung"))
-
-    root.add_child(laptops)
-    root.add_child(cellphones)
-    root.add_child(tvs)
-
-    return root
+    nilpul = TreeNode("Nilpul", "CEO")
+    chinmay = nilpul.add_child(TreeNode("Chinmay", "CTO"))
+    vishwa = chinmay.add_child(TreeNode("Vishwa", "Infrastructure Head"))
+    vishwa.add_child(TreeNode("Dhaval", "Cloud Engineer"))
+    vishwa.add_child(TreeNode("Abhijit", "App Engineer"))
+    chinmay.add_child(TreeNode("Aamir", "Application Head"))
+    gels = nilpul.add_child(TreeNode("Gels", "HR Head"))
+    gels.add_child(TreeNode("Peter", "Recruitment Manager"))
+    gels.add_child(TreeNode("Waqas", "Policy Manager"))
+    return nilpul
 
 
 if __name__ == '__main__':
     root = build_product_tree()
-    root.print_tree()
+    root.print_tree("both", 3)
