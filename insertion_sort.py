1451e28a52e59c7195e138d7418e861b82b9e7d5
sukurcf <sukurcf@gmail.com>
2021-03-28 15:00:50 +0530

    Implemented insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(elements):
+    for i in range(len(elements)):
+        anchor = elements[i]
+        j = i - 1
+        while anchor < elements[j] and j >= 0:
+            elements[j + 1] = elements[j]
+            j -= 1
+        elements[j+1] = anchor
+
+
+if __name__ == '__main__':
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    insertion_sort(elements)
+    print(elements)
