7749df5ff2b4e31b995b92bf3c08337304070f0d
sukurcf <sukurcf@gmail.com>
2021-04-03 16:12:47 +0530

    Implemented shell sort

--- shell_sort.py
+++ shell_sort.py
@@ -11,7 +11,31 @@ def shell_sort(elements):
         gap //= 2
 
 
+def shell_sort_remove_duplicates(elements):
+    size = len(elements)
+    div = 2
+    gap = size // div
+
+    while gap > 0:
+        indexs_to_delete = []
+        for i in range(gap, size):
+            anchor = elements[i]
+            j = i
+            while anchor <= elements[j - gap] and j >= gap:
+                if anchor == elements[j - gap]:
+                    indexs_to_delete.append(j)
+                elements[j] = elements[j - gap]
+                j -= gap
+            elements[j] = anchor
+        if indexs_to_delete:
+            for i in indexs_to_delete:
+                del elements[i]
+        size = len(elements)
+        div *= 2
+        gap = size // div
+
+
 if __name__ == '__main__':
-    elements = [21, 38, 29, 17, 4, 25, 11, 32, 9]
-    shell_sort(elements)
+    elements = [21, 38, 29, 17, 4, 25, 11, 32, 9, 9]
+    shell_sort_remove_duplicates(elements)
     print(elements)
